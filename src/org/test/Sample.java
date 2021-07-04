e8d4c4835a600674fcc515796b1be965ee5a71a1
Suguna121 <vasusuguna@gmail.com>
2021-07-04 13:08:32 +0530

    added to master

---
+++ src/org/test/Sample.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Sample {
+private void empId() {
+	System.out.println("Employee Id is 5561");
+
+}
+public static void main(String[] args) {
+	Sample s=new Sample();
+	s.empId();
+}
+}
